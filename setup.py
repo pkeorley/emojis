9035d7705393202124ef46d2811b74990085092b
pkeorley <nerfinyt@gmail.com>
2023-05-08 14:22:32 +0200

    build: 1.0.0 - setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='emojis',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/pkeorley/emojis',
+    license='Apache License 2.0',
+    author='pkeorley',
+    author_email='nerfinyt@gmail.com',
+    description='Parse and download all emojis from discadia.com'
+)
